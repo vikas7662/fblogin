0b011295910449a79841fd8dd35b604982f17fe3
vikas7662 <vikas.pandey@iglobsyn.com>
2017-06-06 16:45:03 +0530

    commit changes
    sacsabasbvdvsadvhasdsadsavdas 132654

--- application/views/user_authentication/index.php
+++ application/views/user_authentication/index.php
@@ -33,7 +33,7 @@ h1{
 </style>
 </head>
 <body>
-<!-- Changes by vikas  -->
+
 <?php
 if(!empty($authUrl)) {
 	echo '<a href="'.$authUrl.'"><img src="'.base_url().'assets/images/flogin.png" alt=""/></a>';
